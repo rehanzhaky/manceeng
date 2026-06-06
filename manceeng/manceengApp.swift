07da183996d0bfc2eede45d9f8e204dd827abf3c
BaBaL30 <babaluwu@gmail.com>
2026-06-06 15:24:56 +0700

    lanjutan push tadi

---
+++ manceeng/manceengApp.swift
@@ -0,0 +1,32 @@
+//
+//  manceengApp.swift
+//  manceeng
+//
+//  Created by Raihan Zhaky Al Hafizh on 26/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct manceengApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
